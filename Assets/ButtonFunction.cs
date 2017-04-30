412c412a07405086d042e4c25f4b7dbcf48f9f5b
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-04-30 13:45:26 +0900

    魔理沙が弾を撃てるようにした。

--- Assets/ButtonFunction.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public class ButtonFunction : MonoBehaviour {
-
-	public void OnStartBtnClicked(){
-		Application.LoadLevel ("MainView");
-	}
-}
