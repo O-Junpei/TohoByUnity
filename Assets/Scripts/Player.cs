2ff2608206c4f21232cb7e18758b5004b023fafe
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-05-03 02:08:14 +0900

    Chap5 弾が当たっても敵が死なない、、、

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
-	// 移動スピード
-	public float speed = 5;
+	// Spaceshipコンポーネント
+	Spaceship spaceship;
 
-	// PlayerBulletプレハブ
-	public GameObject bullet;
-
-	// Startメソッドをコルーチンとして呼び出す
 	IEnumerator Start ()
 	{
+		// Spaceshipコンポーネントを取得
+		spaceship = GetComponent<Spaceship> ();
+
 		while (true) {
+
 			// 弾をプレイヤーと同じ位置/角度で作成
-			Instantiate (bullet, transform.position, transform.rotation);
-			// 0.05秒待つ
-			yield return new WaitForSeconds (0.05f);
+			spaceship.Shot (transform);
+
+			// shotDelay秒待つ
+			yield return new WaitForSeconds (spaceship.shotDelay);
 		}
 	}
 
@@ -32,7 +34,31 @@ public class Player : MonoBehaviour
 		// 移動する向きを求める
 		Vector2 direction = new Vector2 (x, y).normalized;
 
-		// 移動する向きとスピードを代入する
-		GetComponent<Rigidbody2D>().velocity = direction * speed;
+		// 移動
+		spaceship.Move (direction);
+	}
+
+	// ぶつかった瞬間に呼び出される
+	void OnTriggerEnter2D (Collider2D c)
+	{
+		// レイヤー名を取得
+		string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+		// レイヤー名がBullet (Enemy)の時は弾を削除
+		if( layerName == "Bullet (Enemy)")
+		{
+			// 弾の削除
+			Destroy(c.gameObject);
+		}
+
+		// レイヤー名がBullet (Enemy)またはEnemyの場合は爆発
+		if( layerName == "Bullet (Enemy)" || layerName == "Enemy")
+		{
+			// 爆発する
+			spaceship.Explosion();
+
+			// プレイヤーを削除
+			Destroy (gameObject);
+		}
 	}
 }
